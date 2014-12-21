22ecd94cfed23488f03925382c39860303c577a2
squirrels <bwaismeyer@gmail.com>
2014-12-21 13:11:40 -0800

    Course solution with notes adjusted for consistency.

--- cachematrix.R
+++ cachematrix.R
@@ -6,32 +6,32 @@
 ## and functions for getting and setting the matrix and inverse.
 
 makeCacheMatrix <- function(cached_matrix = matrix()) {
-  # the matrix was defined in the function args above
-  # here we give the initial definition of the matrix inverse 
-  #(NULL because we don't yet know it)
+  # the cached matrix was defined in the function args above
+  # here we give an initial definition of the matrix inverse 
+  # (set to NULL because we don't know the inverse yet)
   cached_inverse <- NULL
   
-  # define functions for interacting with the matrix data
+  # define functions for interacting with the cached matrix
   set_matrix <- function(matrix_data) {
     cached_matrix  <<- matrix_data
     cached_inverse <<- NULL
   }
   get_matrix <- function() cached_matrix
   
-  # define functions for interacting with the matrix inverse
+  # define functions for interacting with the cached inverse
   set_inverse <- function(matrix_inverse) {
     cached_inverse <<- matrix_inverse
   }
   get_inverse <- function() cached_inverse
   
-  # return list of functions for interacting with our matrix/inverse object
+  # return list of functions for interacting with our special matrix object
   list(set_matrix = set_matrix, get_matrix = get_matrix,
        set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
 ## This function interacts with our special matrix object.
-## It checks to see if the inverse has been set yet.
+## It checks to see if the inverse has been calculated yet.
 ## If not, it works on the cached matrix and finds and caches its inverse.
 
 cacheSolve <- function(matrix_object, ...) {
